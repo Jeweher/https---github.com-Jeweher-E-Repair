d8ee8567a36c15d7eebfd121e4f744df16489ed5
Jeweher <mbarekjeweher824@gmail.com>
2024-03-10 20:40:45 +0100

    validation : page login et register

--- src/app/componets/login/login.component.ts
+++ src/app/componets/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
+      Email: ['', Validators.required],
       password: ['', Validators.required],
     })
 
--- src/app/componets/signup/signup.component.ts
+++ src/app/componets/signup/signup.component.ts
@@ -1,86 +1,141 @@
-import { Component ,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent   implements OnInit {
+export class SignupComponent implements OnInit {
 
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
 
   role: string = '';
-  constructor(){}
-  
-  
-  
-  
+  constructor(private fb: FormBuilder,) { }
+
+
+
+
   ngOnInit(): void {
-   
+
+    this.signUpForm = this.fb.group({
+
+      Nom: ['', Validators.required],
+      Prénom: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      Email: ['', Validators.required],
+      Password: ['', Validators.required],
+      Numérodetélépone: ['', Validators.required],
+
+
+      Adresse: ['', Validators.required],
+      role: ['', Validators.required],
+
+    })
+
   }
 
 
-  hideShowPass(){
+
+
+  hideShowPass() {
     this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon= "fa-eye-slash";
-    this.isText ? this.type = "text" : this.type= "password";
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
 
 
   }
+
+
+
+
+
+
+
+
+
+
+
+  public listItems: Array<string> = [
+    "Menuisier",
+    "Plombier",
+    "Électricien",
+    "Charpentier",
+    "Couvreur",
+    "Peintre",
+    "Serrurier",
+    "Vitrier",
+    "Carreleur",
+    "Plâtrier",
+    "Chauffagiste",
+    "Climatiseur",
+    "Jardinier",
+    "Menuisier aluminium",
+    "Ferronnier",
+    "Soudeur",
+    "Isolateur",
+    "Pisciniste",
+    "Déménageur",
+    "Nettoyant",
+    "Bricoleur",
+  ];
+  public value: any = [''];
+
   public areaList: Array<string> = [
     'Ariana',
-  'Beja',
-  'Ben Arous',
-  'Gabes',
-  'Gafsa',
-  'Jendouba',
-  'Kairouan',
-  'Kasserine',
-  'Kebili',
-  'La Manouba',
-  'Mahdia',
-  'Medenine',
-  'Monastir',
-  'Nabeul',
-  'Sfax',
-  'Sidi Bouzid',
-  'Siliana',
-  'Sousse',
-  'Tataouine',
-  'Tozeur',
-  'Tunis',
-  'Zaghouan',
-  'El Kef',
-  'Bizerte'
-];
+    'Beja',
+    'Ben Arous',
+    'Gabes',
+    'Gafsa',
+    'Jendouba',
+    'Kairouan',
+    'Kasserine',
+    'Kebili',
+    'La Manouba',
+    'Mahdia',
+    'Medenine',
+    'Monastir',
+    'Nabeul',
+    'Sfax',
+    'Sidi Bouzid',
+    'Siliana',
+    'Sousse',
+    'Tataouine',
+    'Tozeur',
+    'Tunis',
+    'Zaghouan',
+    'El Kef',
+    'Bizerte'
+  ];
 
 
-public listItems: Array<string> = [
-  "Menuisier",
-  "Plombier",
-  "Électricien",
-  "Charpentier",
-  "Couvreur",
-  "Peintre",
-  "Serrurier",
-  "Vitrier",
-  "Carreleur",
-  "Plâtrier",
-  "Chauffagiste",
-  "Climatiseur",
-  "Jardinier",
-  "Menuisier aluminium",
-  "Ferronnier",
-  "Soudeur",
-  "Isolateur",
-  "Pisciniste",
-  "Déménageur",
-  "Nettoyant",
-  "Bricoleur",
-];
-public value: any = [''];
 
 
+
+  onSignUp() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value)
+    } else {
+      this.validateAllFormFileds(this.signUpForm)
+
+    }
+
+
+
+  }
+
+  private validateAllFormFileds(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFileds(control);
+      }
+    });
+
+  }
 }
